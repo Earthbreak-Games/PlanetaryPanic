fc56e8a6bafe5fc317f8ff07a51128ddff061c1c
henry.chronowski <hrchronowski@gmail.com>
2023-10-17 20:15:40 -0400

    Initial Project Setup

---
+++ PlanetaryPanic/Source/PlanetaryPanic/PlanetaryPanicPlayerController.cpp
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "PlanetaryPanicPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+
+void APlanetaryPanicPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// get the enhanced input subsystem
+	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+	{
+		// add the mapping context so we get controls
+		Subsystem->AddMappingContext(InputMappingContext, 0);
+
+		UE_LOG(LogTemp, Warning, TEXT("BeginPlay"));
+	}
+}
